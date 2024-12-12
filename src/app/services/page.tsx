041a12252e731c5cbe7af2c889ae5764d8273d75
Wineshuga <uzochukwuwinnie@gmail.com>
2024-12-12 23:05:17 +0100

    Adjust padding to fit cards

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -22,7 +22,7 @@ export default function Services() {
           </p>
         </article>
       </section>
-      <section className="bg-[#f7f7f5] px-10 py-36">
+      <section className="bg-[#f7f7f5] md:px-10 px-5 py-28 md:py-36">
         <section className="grid lg:grid-cols-4 min-[480px]:grid-cols-2 grid-cols-1 md:gap-8 gap-5 justify-center place-items-center min-[480px]:place-items-stretch">
           <article className="min-[480px]:col-span-2 flex flex-col justify-center">
             <p className="font-semibold text-xl italic">
