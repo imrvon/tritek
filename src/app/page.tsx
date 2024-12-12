220c08a074895ad93b14aa66941dbc8db7237662
imrvon <webdesignervon@gmail.com>
2024-12-12 12:02:07 +0100

    Add: About Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
 import Marquee from './components/Marquee/Marquee'
 import HomeAbout from './components/HomeAbout/HomeAbout'
 import Testimonials from './components/Testimonials/Testimonials'
-import About from './components/About/About'
 
 export default function page() {
   return (
@@ -13,7 +11,6 @@ export default function page() {
         <Marquee />
         <HomeAbout />
         <Testimonials />
-        <About />
     </div>
   )
 }
